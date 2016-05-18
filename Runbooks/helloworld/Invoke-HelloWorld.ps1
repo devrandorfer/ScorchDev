f1a424092ab2b70ccc4f926d454939ae4c25b781
Ryan Andorfer <ryan.andorfer@gmail.com>
2016-05-18 10:26:13 -0500

    create invoke helloworld

---
+++ Runbooks/helloworld/Invoke-HelloWorld.ps1
@@ -0,0 +1,43 @@
+﻿<#
+    .SYNOPSIS
+       Add a synopsis here to explain the PSScript. 
+
+    .Description
+        Give a description of the Script.
+
+#>
+Param(
+
+)
+$ErrorActionPreference = [System.Management.Automation.ActionPreference]::Stop
+$CompletedParameters = Write-StartingMessage -CommandName Invoke-HelloWorld
+
+$GlobalVars = Get-BatchAutomationVariable -Name  'DomainCredentialName' `
+                                          -Prefix 'Global'
+
+$Vars = Get-BatchAutomationVariable -Prefix 'HelloWOrld' `
+                                    -Name @(
+                                        'Audience'
+                                        'Message'
+                                    )
+
+$Credential = Get-AutomationPSCredential -Name $GlobalVars.DomainCredentialName
+
+Try
+{
+    Write-Verbose -Message "Hello $($Vars.Audience) $($Vars.Message)"
+}
+Catch
+{
+    $Exception = $_
+    $ExceptionInfo = Get-ExceptionInfo -Exception $Exception
+    Switch ($ExceptionInfo.FullyQualifiedErrorId)
+    {
+        Default
+        {
+            Write-Exception $Exception -Stream Warning
+        }
+    }
+}
+
+Write-CompletedMessage @CompletedParameters
