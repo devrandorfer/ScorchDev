1253970807c063ace9fe6dbd31c8c18f9d54b5d2
Ryan Andorfer <ryan.andorfer@gmail.com>
2016-06-01 13:32:37 -0500

    create

---
+++ Runbooks/BackupVault/New-BackupVault.ps1
@@ -0,0 +1,63 @@
+﻿<#
+    .SYNOPSIS
+       Add a synopsis here to explain the PSScript. 
+
+    .Description
+        Give a description of the Script.
+
+#>
+Param(
+
+)
+$ErrorActionPreference = [System.Management.Automation.ActionPreference]::Stop
+$CompletedParameters = Write-StartingMessage -CommandName New-BackupVault
+
+$GlobalVars = Get-BatchAutomationVariable -Prefix 'zzGlobal' `
+                                          -Name 'SubscriptionName',
+                                                'SubscriptionAccessCredentialName',
+                                                'SubscriptionAccessTenant'
+
+$SubscriptionAccessCredential = Get-AutomationPSCredential -Name $GlobalVars.SubscriptionAccessCredentialName
+
+
+Try
+{
+    Connect-AzureRmAccount -Credential $SubscriptionAccessCredential `
+                           -SubscriptionName $GlobalVars.SubscriptionName `
+                           -Tenant $GlobalVars.SubscriptionAccessTenant
+
+    $ResourceGroupName = 'AUtomationDemo'
+    $VaultName = 'scorchDemoVault'
+    $PolicyName = 'Weekly'
+    New-AzureRmResourcegroup -Name $ResourceGroupName `
+                             -Location 'EastUS' `
+                             -Verbose `
+                             -Force
+
+    New-AzureRmResourceGroupDeployment -Name InitialDeployment `
+                                       -TemplateFile 'C:\git\ScorchDev\ARM\101-recovery-services-vault-create\azuredeploy.json' `
+                                       -vaultName 'scorchDemoVault' `
+                                       -ResourceGroupName $ResourceGroupName `
+                                       -Verbose
+
+    New-AzureRmResourceGroupDeployment -Name CreateBackupPolicy `
+                                       -TemplateFile 'C:\git\ScorchDev\ARM\101-recovery-services-weekly-backup-policy-create\azuredeploy.json' `
+                                       -vaultName $VaultName `
+                                       -TemplateParameterFile 'C:\git\ScorchDev\ARM\101-recovery-services-weekly-backup-policy-create\azuredeploy.parameters.json' `
+                                       -ResourceGroupName $ResourceGroupName `
+                                       -Verbose
+}
+Catch
+{
+    $Exception = $_
+    $ExceptionInfo = Get-ExceptionInfo -Exception $Exception
+    Switch ($ExceptionInfo.FullyQualifiedErrorId)
+    {
+        Default
+        {
+            Write-Exception $Exception -Stream Warning
+        }
+    }
+}
+
+Write-CompletedMessage @CompletedParameters
---
+++ Runbooks/WindowsServerDeployment/New-WindowsServerDeployment.ps1
@@ -0,0 +1,61 @@
+﻿<#
+    .SYNOPSIS
+       Add a synopsis here to explain the PSScript. 
+
+    .Description
+        Give a description of the Script.
+
+#>
+Param(
+
+)
+$ErrorActionPreference = [System.Management.Automation.ActionPreference]::Stop
+$CompletedParameters = Write-StartingMessage -CommandName New-WindowsServerDeployment
+
+$GlobalVars = Get-BatchAutomationVariable -Prefix 'zzGlobal' `
+                                          -Name 'SubscriptionName',
+                                                'SubscriptionAccessCredentialName',
+                                                'ResourceGroupName',
+                                                'SubscriptionAccessTenant',
+                                                'RunbookWorkerAccessCredentialName'
+
+$SubscriptionAccessCredential = Get-AutomationPSCredential -Name $GlobalVars.SubscriptionAccessCredentialName
+$RunbookWorkerAccessCredential = Get-AutomationPSCredential -Name $GlobalVars.RunbookWorkerAccessCredentialName
+
+
+Try
+{
+    Connect-AzureRmAccount -Credential $SubscriptionAccessCredential `
+                           -SubscriptionName $GlobalVars.SubscriptionName `
+                           -Tenant $GlobalVars.SubscriptionAccessTenant
+
+    $VMName = "$(New-RandomString -MinLength 5 -MaxLength 5 -InputString 'abcdefghijklmnopqrstuvwxyz')"
+    $ResourceGroupName = 'AUtomationDemo'
+
+    New-AzureRmResourcegroup -Name $ResourceGroupName `
+                             -Location 'EastUS' `
+                             -Verbose `
+                             -Force
+
+    New-AzureRmResourceGroupDeployment -Name InitialDeployment `
+                                       -TemplateFile 'C:\git\ScorchDev\ARM\101-vm-simple-windows\azuredeploy.json' `
+                                       -adminUsername $RunbookWorkerAccessCredential.UserName `
+                                       -adminPassword $RunbookWorkerAccessCredential.Password `
+                                       -dnsLabelPrefix $VMName `
+                                       -ResourceGroupName $ResourceGroupName `
+                                       -Verbose
+}
+Catch
+{
+    $Exception = $_
+    $ExceptionInfo = Get-ExceptionInfo -Exception $Exception
+    Switch ($ExceptionInfo.FullyQualifiedErrorId)
+    {
+        Default
+        {
+            Write-Exception $Exception -Stream Warning
+        }
+    }
+}
+
+Write-CompletedMessage @CompletedParameters
