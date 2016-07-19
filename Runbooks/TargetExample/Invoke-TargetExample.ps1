2a2d04b1f850c22e76a11c63edafc2aab8d71452
Ryan Andorfer <ryan.andorfer@gmail.com>
2016-07-19 11:41:39 -0500

    new checkin for demo

---
+++ Runbooks/TargetExample/Invoke-TargetExample.ps1
@@ -0,0 +1,40 @@
+﻿
+<#
+    .SYNOPSIS
+       Add a synopsis here to explain the PSScript. 
+
+    .Description
+        Give a description of the Script.
+
+#>
+Param(
+
+)
+$ErrorActionPreference = [System.Management.Automation.ActionPreference]::Stop
+$CompletedParameters = Write-StartingMessage -CommandName Invoke-TargetExample.ps1
+
+$GlobalVars = Get-BatchAutomationVariable -Prefix 'TargetExample' `
+                                          -Name 'HelloMessage',
+                                                'Audience'
+
+$SubscriptionAccessCredential = Get-AutomationPSCredential -Name 'ryan'
+
+
+Try
+{
+    Write-Verbose -Message "$($GlobalVars.HelloMessage)$($GlobalVars.Audience)"
+}
+Catch
+{
+    $Exception = $_
+    $ExceptionInfo = Get-ExceptionInfo -Exception $Exception
+    Switch ($ExceptionInfo.FullyQualifiedErrorId)
+    {
+        Default
+        {
+            Write-Exception $Exception -Stream Warning
+        }
+    }
+}
+
+Write-CompletedMessage @CompletedParameters
