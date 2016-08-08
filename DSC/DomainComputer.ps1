26d223356cfe3daea2ae6460c254e4e05b6b27ea
Ryan Andorfer <ryan.andorfer@gmail.com>
2016-08-08 14:53:10 -0500

    update Bluestripe to 8.2.4

--- DSC/DomainComputer.ps1
+++ DSC/DomainComputer.ps1
@@ -29,7 +29,7 @@
         "OPINSIGHTS_WORKSPACE_ID=$($GlobalVars.WorkspaceID) " +
         "OPINSIGHTS_WORKSPACE_KEY=$($WorkspaceKey)`""
 
-    $ADMVersion = '8.2.2'
+    $ADMVersion = '8.2.4'
     $ADMRemotSetupExeURI = 'https://go.microsoft.com/fwlink/?LinkId=698625'
     $ADMSetupExe = 'ADM-Agent-Windows.exe'
     $ADMCommandLineArguments = '/S'
