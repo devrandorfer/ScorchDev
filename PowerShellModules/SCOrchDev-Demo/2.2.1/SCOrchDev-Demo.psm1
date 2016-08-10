7748b2352e8d1171e48cad91853cec608c4e869f
Ryan.Andorfer <Ryan.Andorfer@microsoft.com>
2016-08-10 10:39:03 -0500

    new module version

---
+++ PowerShellModules/SCOrchDev-Demo/2.2.1/SCOrchDev-Demo.psm1
@@ -0,0 +1,13 @@
+﻿Function Invoke-Step1
+{
+    WRite-Verbose -Message 'Step 1'
+}
+Function Invoke-Step2
+{
+    WRite-Verbose -Message 'Step 2'
+}
+Function Invoke-Step3
+{
+    WRite-Verbose -Message 'Step 3'
+}
+Export-ModuleMember -Function * -Alias * -Verbose:$False
