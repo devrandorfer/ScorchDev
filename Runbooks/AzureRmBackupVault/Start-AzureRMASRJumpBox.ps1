9977d75ade184fec933f0bcd8a637725a0707150
Ryan.Andorfer <Ryan.Andorfer@microsoft.com>
2016-08-15 08:22:25 -0500

    update

---
+++ Runbooks/AzureRmBackupVault/Start-AzureRMASRJumpBox.ps1
@@ -0,0 +1,51 @@
+﻿<#
+    .SYNOPSIS
+       Add a synopsis here to explain the PSScript
+
+    .Description
+        Give a description of the Script.
+
+#>
+Param(
+
+)
+$ErrorActionPreference = [System.Management.Automation.ActionPreference]::Stop
+$CompletedParameters = Write-StartingMessage -CommandName Start-AzureRMASRJumpBox
+
+$GlobalVars = Get-BatchAutomationVariable -Prefix 'zzGlobal' `
+                                          -Name 'SubscriptionName',
+                                                'SubscriptionAccessCredentialName',
+                                                'ResourceGroupName',
+                                                'SubscriptionAccessTenant'
+
+$Vars = Get-BatchAutomationVariable -Prefix 'AzureRMBackupLog' `
+                                    -Name @(
+                                        'LastSaveDateTime'
+                                        'LogPath'
+                                    )
+
+$SubscriptionAccessCredential = Get-AutomationPSCredential -Name $GlobalVars.SubscriptionAccessCredentialName
+
+
+Try
+{
+    Connect-AzureRmAccount -Credential $SubscriptionAccessCredential `
+                           -SubscriptionName $GlobalVars.SubscriptionName `
+                           -Tenant $GlobalVars.SubscriptionAccessTenant
+    
+    Start-AzureRMVM -Name 'TestHopBox' -ResourceGroupName 'West-US-Recovery'
+}
+Catch
+{
+    $Exception = $_
+    $ExceptionInfo = Get-ExceptionInfo -Exception $Exception
+    Switch ($ExceptionInfo.FullyQualifiedErrorId)
+    {
+        Default
+        {
+            Write-Exception $Exception -Stream Warning
+        }
+    }
+}
+
+Write-CompletedMessage @CompletedParameters
