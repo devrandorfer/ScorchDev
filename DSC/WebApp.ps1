2e9c26b6e5039c80c8c64bcb10009ee4ca269d30
Ryan.Andorfer <Ryan.Andorfer@microsoft.com>
2016-08-15 07:38:20 -0500

    new config

---
+++ DSC/WebApp.ps1
@@ -0,0 +1,210 @@
+﻿Configuration WebApp
+{
+    Import-DscResource -Module xPSDesiredStateConfiguration
+    Import-DscResource -Module PSDesiredStateConfiguration
+    Import-DscResource -Module cWindowscomputer
+    Import-DscResource -Module cAzureAutomation
+    Import-DscResource -Module xPendingReboot
+    Import-DscResource -Module xDSCDomainjoin -ModuleVersion 1.1
+    Import-DscResource -Module xWebAdministration
+    Import-DscResource -Module cNetworkAdapter
+    Import-DscResource -Module cDisk
+    Import-DscResource -Module xDisk
+
+    $SourceDir = 'D:\Source'
+    $GlobalVars = Get-BatchAutomationVariable -Prefix 'zzGlobal' `
+                                              -Name @(
+        'WorkspaceID',
+        'DomainJoinCredentialName',
+        'DomainName'
+    )
+
+    $DomainJoinCredential = Get-AutomationPSCredential -Name $GlobalVars.DomainJoinCredentialName
+    
+    $WorkspaceCredential = Get-AutomationPSCredential -Name $GlobalVars.WorkspaceID
+    $WorkspaceKey = $WorkspaceCredential.GetNetworkCredential().Password
+
+    $MMARemotSetupExeURI = 'https://go.microsoft.com/fwlink/?LinkID=517476'
+    $MMASetupExe = 'MMASetup-AMD64.exe'
+    
+    $MMACommandLineArguments = 
+        '/Q /C:"setup.exe /qn ADD_OPINSIGHTS_WORKSPACE=1 AcceptEndUserLicenseAgreement=1 ' +
+        "OPINSIGHTS_WORKSPACE_ID=$($GlobalVars.WorkspaceID) " +
+        "OPINSIGHTS_WORKSPACE_KEY=$($WorkspaceKey)`""
+
+    $ADMVersion = '8.2.4'
+    $ADMRemotSetupExeURI = 'https://go.microsoft.com/fwlink/?LinkId=698625'
+    $ADMSetupExe = 'ADM-Agent-Windows.exe'
+    $ADMCommandLineArguments = '/S'
+
+    $MicrosoftAzureSiteRecoveryUnifiedSetupURI = 'http://aka.ms/unifiedinstaller'
+    $ASRSetupEXE = 'MicrosoftAzureSiteRecoveryUnifiedSetup.exe'
+
+    $RetryCount = 20
+    $RetryIntervalSec = 30
+
+    Node FrontEnd
+    {   
+        xWaitforDisk DataDisk
+        {
+                DiskNumber = 2
+                RetryIntervalSec =$RetryIntervalSec
+                RetryCount = $RetryCount
+        }
+        cDiskNoRestart DataDisk
+        {
+            DiskNumber = 2
+            DriveLetter = 'F'
+        }
+        File SourceFolder
+        {
+            DestinationPath = $($SourceDir)
+            Type = 'Directory'
+            Ensure = 'Present'
+        }
+        xRemoteFile DownloadMicrosoftManagementAgent
+        {
+            Uri = $MMARemotSetupExeURI
+            DestinationPath = "$($SourceDir)\$($MMASetupExe)"
+            MatchSource = $False
+        }
+        xPackage InstallMicrosoftManagementAgent
+        {
+             Name = "Microsoft Monitoring Agent"
+             Path = "$($SourceDir)\$($MMASetupExE)" 
+             Arguments = $MMACommandLineArguments 
+             Ensure = 'Present'
+             InstalledCheckRegKey = 'SOFTWARE\Microsoft\Microsoft Operations Manager\3.0\Setup'
+             InstalledCheckRegValueName = 'Product'
+             InstalledCheckRegValueData = 'Microsoft Monitoring Agent'
+             ProductID = ''
+             DependsOn = "[xRemoteFile]DownloadMicrosoftManagementAgent"
+        }
+        xPendingReboot Reboot1
+        { 
+            Name = "RebootServer"
+            DependsOn = "[xPackage]InstallMicrosoftManagementAgent"
+        }
+        xRemoteFile DownloadAppDependencyMonitor
+        {
+            Uri = $ADMRemotSetupExeURI
+            DestinationPath = "$($SourceDir)\$($ADMSetupExe)"
+            MatchSource = $False
+        }
+        xPackage InstallAppDependencyMonitor
+        {
+             Name = "Application Dependency Monitor"
+             Path = "$($SourceDir)\$($ADMSetupExE)" 
+             Arguments = $ADMCommandLineArguments 
+             Ensure = 'Present'
+             InstalledCheckRegKey = 'SOFTWARE\Wow6432Node\Microsoft\Windows\CurrentVersion\Uninstall\DependencyAgent'
+             InstalledCheckRegValueName = 'DisplayVersion'
+             InstalledCheckRegValueData = $ADMVersion
+             ProductID = ''
+             DependsOn = "[xRemoteFile]DownloadMicrosoftManagementAgent"
+        }
+        xPendingReboot Reboot2
+        { 
+            Name = "RebootServer2"
+            DependsOn = "[xPackage]InstallAppDependencyMonitor"
+        }
+        xDSCDomainjoin JoinDomain
+        {
+            Domain = $GlobalVars.DomainName
+            Credential = $DomainJoinCredential
+        }
+        # Install the IIS role
+        WindowsFeature IIS
+        {
+            Ensure          = 'Present'
+            Name            = 'Web-Server'
+        }
+
+        # Install the ASP .NET 4.5 role
+        WindowsFeature AspNet45
+        {
+            Ensure          = 'Present'
+            Name            = 'Web-Asp-Net45'
+        }
+
+        # Setup the default website
+        xWebsite DefaultSite 
+        {
+            Ensure          = 'Present'
+            Name            = 'Default Web Site'
+            State           = 'Started'
+            PhysicalPath    = 'F:\inetpub\wwwroot'
+            DependsOn       = '[WindowsFeature]IIS'
+        }
+        cAzureNetworkPerformanceMonitoring EnableAzureNPM
+        {
+            Name = 'EnableNPM'
+            Ensure = 'Present'
+        }
+    }
+    Node SQL
+    {
+        File SourceFolder
+        {
+            DestinationPath = $($SourceDir)
+            Type = 'Directory'
+            Ensure = 'Present'
+        }
+        xRemoteFile DownloadMicrosoftManagementAgent
+        {
+            Uri = $MMARemotSetupExeURI
+            DestinationPath = "$($SourceDir)\$($MMASetupExe)"
+            MatchSource = $False
+        }
+        xPackage InstallMicrosoftManagementAgent
+        {
+             Name = "Microsoft Monitoring Agent"
+             Path = "$($SourceDir)\$($MMASetupExE)" 
+             Arguments = $MMACommandLineArguments 
+             Ensure = 'Present'
+             InstalledCheckRegKey = 'SOFTWARE\Microsoft\Microsoft Operations Manager\3.0\Setup'
+             InstalledCheckRegValueName = 'Product'
+             InstalledCheckRegValueData = 'Microsoft Monitoring Agent'
+             ProductID = ''
+             DependsOn = "[xRemoteFile]DownloadMicrosoftManagementAgent"
+        }
+        xPendingReboot Reboot1
+        { 
+            Name = "RebootServer"
+            DependsOn = "[xPackage]InstallMicrosoftManagementAgent"
+        }
+        xRemoteFile DownloadAppDependencyMonitor
+        {
+            Uri = $ADMRemotSetupExeURI
+            DestinationPath = "$($SourceDir)\$($ADMSetupExe)"
+            MatchSource = $False
+        }
+        xPackage InstallAppDependencyMonitor
+        {
+             Name = "Application Dependency Monitor"
+             Path = "$($SourceDir)\$($ADMSetupExE)" 
+             Arguments = $ADMCommandLineArguments 
+             Ensure = 'Present'
+             InstalledCheckRegKey = 'SOFTWARE\Wow6432Node\Microsoft\Windows\CurrentVersion\Uninstall\DependencyAgent'
+             InstalledCheckRegValueName = 'DisplayVersion'
+             InstalledCheckRegValueData = $ADMVersion
+             ProductID = ''
+             DependsOn = "[xRemoteFile]DownloadMicrosoftManagementAgent"
+        }
+        xPendingReboot Reboot2
+        { 
+            Name = "RebootServer2"
+            DependsOn = "[xPackage]InstallAppDependencyMonitor"
+        }
+        xDSCDomainjoin JoinDomain
+        {
+            Domain = $GlobalVars.DomainName
+            Credential = $DomainJoinCredential
+        }
+        cAzureNetworkPerformanceMonitoring EnableAzureNPM
+        {
+            Name = 'EnableNPM'
+            Ensure = 'Present'
+        }
+    }
+}
