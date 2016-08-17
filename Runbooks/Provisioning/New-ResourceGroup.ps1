42e2c93becce176dbffd7e430b5b7559db886fde
Ryan.Andorfer <Ryan.Andorfer@microsoft.com>
2016-08-17 11:24:34 -0500

    new demo script

---
+++ Runbooks/Provisioning/New-ResourceGroup.ps1
@@ -0,0 +1,47 @@
+﻿<#
+    .SYNOPSIS
+       Add a synopsis here to explain the PSScript. 
+
+    .Description
+        Give a description of the Script.
+
+#>
+Param(
+    $ResourceGroupName,
+    $SignInName
+)
+$ErrorActionPreference = [System.Management.Automation.ActionPreference]::Stop
+$CompletedParameters = Write-StartingMessage -CommandName New-ResourceGroup
+
+$GlobalVars = Get-BatchAutomationVariable -Prefix 'zzGlobal' `
+                                          -Name 'SubscriptionName',
+                                                'SubscriptionAccessCredentialName'
+
+$SubscriptionAccessCredential = Get-AutomationPSCredential -Name $GlobalVars.SubscriptionAccessCredentialName
+
+
+Try
+{
+    Connect-AzureRmAccount -Credential $SubscriptionAccessCredential `
+                           -SubscriptionName $GlobalVars.SubscriptionName
+
+    $ResourceGroup = New-AzureRmResourceGroup -Name $ResourceGroupName -Location 'eastus2'
+    New-AzureRMRoleAssignment -ResourceGroupName $ResourceGroupName `
+                              -RoleDefinitionName 'Contributor' `
+                              -SignInName $SignInName | Out-Null
+    
+}
+Catch
+{
+    $Exception = $_
+    $ExceptionInfo = Get-ExceptionInfo -Exception $Exception
+    Switch ($ExceptionInfo.FullyQualifiedErrorId)
+    {
+        Default
+        {
+            Write-Exception $Exception -Stream Warning
+        }
+    }
+}
+
+Write-CompletedMessage @CompletedParameters
