d2d46a4a4f44650c08b16675ff8a416956ee4f65
Ryan Andorfer <Ryan.Andorfer@gmail.com>
2016-08-22 11:27:16 -0500

    updates

---
+++ Runbooks/Monitoring/Save-AzureRMEvent.ps1
@@ -0,0 +1,35 @@
+﻿<#
+    .SYNOPSIS
+       Forwards audit events to Log Analytics real time 
+#>
+Param(
+    [Object]    
+    $WebhookData
+)
+$ErrorActionPreference = [System.Management.Automation.ActionPreference]::Stop
+$CompletedParameters = Write-StartingMessage -CommandName Save-AzureRMEvent
+
+$GlobalVars = Get-BatchAutomationVariable -Prefix 'zzGlobal' `
+                                          -Name 'WorkspaceId'
+
+$WorkspaceCredential = Get-AutomationPSCredential -Name $GlobalVars.WorkspaceID
+$Key = $WorkspaceCredential.GetNetworkCredential().Password
+
+Try
+{
+    $WebhookData
+}
+Catch
+{
+    $Exception = $_
+    $ExceptionInfo = Get-ExceptionInfo -Exception $Exception
+    Switch ($ExceptionInfo.FullyQualifiedErrorId)
+    {
+        Default
+        {
+            Write-Exception $Exception -Stream Warning
+        }
+    }
+}
+
+Write-CompletedMessage @CompletedParameters
