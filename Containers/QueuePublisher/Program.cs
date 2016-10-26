b11948d27e5ed4cfb0cbbe6004c0a73229895513
Ryan Andorfer <ryan@microsoft.com>
2016-10-26 16:21:12 -0500

    update

--- Containers/QueuePublisher/Program.cs
+++ Containers/QueuePublisher/Program.cs
@@ -3,6 +3,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Microsoft.Azure; // Namespace for CloudConfigurationManager
+using Microsoft.WindowsAzure.Storage; // Namespace for CloudStorageAccount
+using Microsoft.WindowsAzure.Storage.Queue;
 
 namespace QueuePublisher
 {
@@ -10,7 +13,32 @@ namespace QueuePublisher
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello WOrld");
+            while (true)
+            {
+                AddMessageinQueue("message", "microscaling-demo");
+                System.Threading.Thread.Sleep(new TimeSpan(0, 0, 5));
+            }
         }
+        public static bool AddMessageinQueue(string MessageToAdd, string QueueName)
+        {
+            try
+            {
+                CloudStorageAccount account = CloudStorageAccount.Parse("DefaultEndpointsProtocol=https;AccountName=microscale;AccountKey=Ue6nybNleYIFDHB382Cn9FbzJDu66ewmIyzO9q5uAhlCe9L8IGEk9ReVp/xp6MQlJrpCeDSmTe/fbe7Lbp97qQ==");
+                CloudQueueClient queueClient = account.CreateCloudQueueClient();
+                CloudQueue queue = queueClient.GetQueueReference(QueueName);
+
+                CloudQueueMessage m = new CloudQueueMessage(MessageToAdd);
+                queue.AddMessage(m);
+
+
+                return true;
+
+            }
+            catch (Exception ex)
+            {
+                return false;
+            }
+        }
+
     }
 }
