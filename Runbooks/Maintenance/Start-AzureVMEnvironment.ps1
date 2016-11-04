b56da9904ae5dbedb9b311ae27b9e32a3fe808e7
Ryan Andorfer <ryan@microsoft.com>
2016-11-04 08:26:53 -0500

    start / stop total vm environment

---
+++ Runbooks/Maintenance/Start-AzureVMEnvironment.ps1
@@ -0,0 +1,33 @@
+﻿<#
+    .SYNOPSIS
+       Add a synopsis here to explain the PSScript. 
+
+    .Description
+        Give a description of the Script.
+
+#>
+Param(
+
+)
+$ErrorActionPreference = [System.Management.Automation.ActionPreference]::Stop
+$CompletedParameters = Write-StartingMessage -CommandName Start-AzureVMEnvironment
+
+Try
+{
+    Start-AzureVmDC
+    Start-AzureVmNonDC
+}
+Catch
+{
+    $Exception = $_
+    $ExceptionInfo = Get-ExceptionInfo -Exception $Exception
+    Switch ($ExceptionInfo.FullyQualifiedErrorId)
+    {
+        Default
+        {
+            Write-Exception $Exception -Stream Warning
+        }
+    }
+}
+
+Write-CompletedMessage @CompletedParameters
