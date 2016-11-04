852f96dd549b8e5c000c7cd9cdec086fa47696fd
Ryan Andorfer <ryan@microsoft.com>
2016-11-04 09:31:33 -0500

    update to make non critial VM start/stop

---
+++ Runbooks/Maintenance/Start-AzureVMNonCritial.ps1
@@ -0,0 +1,58 @@
+﻿<#
+    .SYNOPSIS
+       Add a synopsis here to explain the PSScript. 
+
+    .Description
+        Give a description of the Script.
+
+#>
+Param(
+
+)
+$ErrorActionPreference = [System.Management.Automation.ActionPreference]::Stop
+$CompletedParameters = Write-StartingMessage -CommandName Start-AzureVMNonCritial
+
+$GlobalVars = Get-BatchAutomationVariable -Prefix 'zzGlobal' `
+                                          -Name 'SubscriptionName',
+                                                'SubscriptionAccessCredentialName'
+
+$Vars = Get-BatchAutomationVariable -Prefix 'AzureVMMaintenance' `
+                                    -Name 'CriticalResourceGroupName'
+
+$SubscriptionAccessCredential = Get-AutomationPSCredential -Name $GlobalVars.SubscriptionAccessCredentialName
+
+
+Try
+{
+    Connect-AzureRmAccount -Credential $SubscriptionAccessCredential `
+                           -SubscriptionName $GlobalVars.SubscriptionName
+
+    Get-AzureRmVM | ? {$_.ResourceGroupName -notin ($Vars.CriticalResourceGroupName | ConvertFrom-Json)} | % {
+        $VM = $_
+        Start-Job -Name $VM.Name -ScriptBlock {
+            $VerbosePreference = $Using:VerbosePreference
+            $SubscriptionAccessCredential = $Using:SubscriptionAccessCredential
+            $GlobalVars = $Using:GlobalVars
+            $VM = $Using:VM
+            Connect-AzureRmAccount -Credential $SubscriptionAccessCredential `
+                               -SubscriptionName $GlobalVars.SubscriptionName
+            $VM | Start-AzureRmVM -ErrorAction SilentlyContinue
+        }
+    }
+
+    Get-Job | Receive-Job -Wait -AutoRemoveJob
+}
+Catch
+{
+    $Exception = $_
+    $ExceptionInfo = Get-ExceptionInfo -Exception $Exception
+    Switch ($ExceptionInfo.FullyQualifiedErrorId)
+    {
+        Default
+        {
+            Write-Exception $Exception -Stream Warning
+        }
+    }
+}
+
+Write-CompletedMessage @CompletedParameters
