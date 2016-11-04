f294317dbe5b1735b14b96f85d69f8049c514e9c
Ryan Andorfer <ryan@microsoft.com>
2016-11-04 08:26:34 -0500

    start runbooks

---
+++ Runbooks/Maintenance/Start-AzureVMNonDC.ps1
@@ -0,0 +1,55 @@
+﻿<#
+    .SYNOPSIS
+       Add a synopsis here to explain the PSScript. 
+
+    .Description
+        Give a description of the Script.
+
+#>
+Param(
+
+)
+$ErrorActionPreference = [System.Management.Automation.ActionPreference]::Stop
+$CompletedParameters = Write-StartingMessage -CommandName Start-AzureVMNonDC
+
+$GlobalVars = Get-BatchAutomationVariable -Prefix 'zzGlobal' `
+                                          -Name 'SubscriptionName',
+                                                'SubscriptionAccessCredentialName'
+
+$SubscriptionAccessCredential = Get-AutomationPSCredential -Name $GlobalVars.SubscriptionAccessCredentialName
+
+
+Try
+{
+    Connect-AzureRmAccount -Credential $SubscriptionAccessCredential `
+                           -SubscriptionName $GlobalVars.SubscriptionName
+
+    Get-AzureRmVM | ? {$_.ResourceGroupName -ne 'DomainController'} | % {
+        $VM = $_
+        Start-Job -Name $VM.Name -ScriptBlock {
+            $VerbosePreference = $Using:VerbosePreference
+            $SubscriptionAccessCredential = $Using:SubscriptionAccessCredential
+            $GlobalVars = $Using:GlobalVars
+            $VM = $Using:VM
+            Connect-AzureRmAccount -Credential $SubscriptionAccessCredential `
+                               -SubscriptionName $GlobalVars.SubscriptionName
+            $VM | Start-AzureRmVM -Force -ErrorAction SilentlyContinue
+        }
+    }
+
+    Get-Job | Receieve-Job -Wait -AutoRemoveJob
+}
+Catch
+{
+    $Exception = $_
+    $ExceptionInfo = Get-ExceptionInfo -Exception $Exception
+    Switch ($ExceptionInfo.FullyQualifiedErrorId)
+    {
+        Default
+        {
+            Write-Exception $Exception -Stream Warning
+        }
+    }
+}
+
+Write-CompletedMessage @CompletedParameters
