40b8c330fa3b857964f3c3c848ea50d19d8c10d0
Ryan Andorfer <ryan@microsoft.com>
2017-01-04 09:23:54 -0600

    updates

---
+++ Runbooks/Demo/Invoke-EcolabDemo.ps1
@@ -0,0 +1,45 @@
+﻿<#
+    .SYNOPSIS
+       Add a synopsis here to explain the PSScript. 
+
+    .Description
+        Give a description of the Script.
+
+#>
+Param(
+
+)
+$ErrorActionPreference = [System.Management.Automation.ActionPreference]::Stop
+$CompletedParameters = Write-StartingMessage -CommandName Invoke-EcolabDemo.ps1
+
+$GlobalVars = Get-BatchAutomationVariable -Prefix 'zzGlobal' `
+                                          -Name 'SubscriptionName',
+                                                'SubscriptionAccessCredentialName',
+                                                'ResourceGroupName',
+                                                'SubscriptionAccessTenant'
+
+$SubscriptionAccessCredential = Get-AutomationPSCredential -Name $GlobalVars.SubscriptionAccessCredentialName
+
+
+Try
+{
+    Connect-AzureRmAccount -Credential $SubscriptionAccessCredential `
+                           -SubscriptionName $GlobalVars.SubscriptionName `
+                           -Tenant $GlobalVars.SubscriptionAccessTenant
+
+    Write-Output -InputObject "Hey Ecolab"
+}
+Catch
+{
+    $Exception = $_
+    $ExceptionInfo = Get-ExceptionInfo -Exception $Exception
+    Switch ($ExceptionInfo.FullyQualifiedErrorId)
+    {
+        Default
+        {
+            Write-Exception $Exception -Stream Warning
+        }
+    }
+}
+
+Write-CompletedMessage @CompletedParameters
