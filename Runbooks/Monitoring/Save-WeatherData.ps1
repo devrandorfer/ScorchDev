2eed12a85cda59c5451573bea721bd1fe4321061
Ryan Andorfer <ryan@microsoft.com>
2017-01-05 08:27:32 -0600

    fix error handling

--- Runbooks/Monitoring/Save-WeatherData.ps1
+++ Runbooks/Monitoring/Save-WeatherData.ps1
@@ -29,20 +29,27 @@ Try
         $DataToSave = @()
         Foreach($_City in ($WeatherVars.LocationsToMonitor | ConvertFrom-JSON))
         {
-            $Weather = Get-WeatherCurrentRaw -City $_City -ApiKey $weathervars.APIKey -Units imperial
-            $WeatherData = @{
-                'Longitude' = $Weather.coord.lon
-                'Latitude' = $Weather.coord.lat
-                'Description' = $Weather.weather.main
-                'Description_Detail' = $Weather.weather.description
-                'Temperature' = $Weather.main.temp
-                'Pressure' = $Weather.main.pressure
-                'Humidity' = $Weather.main.humidity
-                'Wind_Speed' = $Weather.wind.speed
-                'Wind_Degree' = $Weather.wind.deg
-                'Location' = $Weather.name
+            try
+            {
+                $Weather = Get-WeatherCurrentRaw -City $_City -ApiKey $weathervars.APIKey -Units imperial
+                $WeatherData = @{
+                    'Longitude' = $Weather.coord.lon
+                    'Latitude' = $Weather.coord.lat
+                    'Description' = $Weather.weather.main
+                    'Description_Detail' = $Weather.weather.description
+                    'Temperature' = $Weather.main.temp
+                    'Pressure' = $Weather.main.pressure
+                    'Humidity' = $Weather.main.humidity
+                    'Wind_Speed' = $Weather.wind.speed
+                    'Wind_Degree' = $Weather.wind.deg
+                    'Location' = $Weather.name
+                }
+                $DataToSave += $WeatherData
             }
-            $DataToSave += $WeatherData
+            catch
+            {
+                Write-Exception -Exception $_ -Stream Warning
+            }
         }
 
         Write-LogAnalyticsLogEntry -WorkspaceId $GlobalVars.WorkspaceId -Key $Key -Data $DataToSave -LogType 'OpenWeather_CL'
