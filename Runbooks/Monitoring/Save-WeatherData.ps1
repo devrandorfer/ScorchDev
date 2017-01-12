7fb67f1cd11005a2804a4a962bf474d46ee5e36c
Ryan Andorfer <ryan@microsoft.com>
2017-01-12 13:30:54 -0600

    move to single run for monitor functionality

--- Runbooks/Monitoring/Save-WeatherData.ps1
+++ Runbooks/Monitoring/Save-WeatherData.ps1
@@ -24,38 +24,33 @@ $Key = $WorkspaceCredential.GetNetworkCredential().Password
 
 Try
 {
-    while($true)
+    $DataToSave = @()
+    Foreach($_City in ($WeatherVars.LocationsToMonitor | ConvertFrom-JSON))
     {
-        $DataToSave = @()
-        Foreach($_City in ($WeatherVars.LocationsToMonitor | ConvertFrom-JSON))
+        try
         {
-            try
-            {
-                $Weather = Get-WeatherCurrentRaw -City $_City -ApiKey $weathervars.APIKey -Units imperial
-                $WeatherData = @{
-                    'Longitude' = $Weather.coord.lon
-                    'Latitude' = $Weather.coord.lat
-                    'Description' = $Weather.weather.main
-                    'Description_Detail' = $Weather.weather.description
-                    'Temperature' = $Weather.main.temp
-                    'Pressure' = $Weather.main.pressure
-                    'Humidity' = $Weather.main.humidity
-                    'Wind_Speed' = $Weather.wind.speed
-                    'Wind_Degree' = $Weather.wind.deg
-                    'Location' = $Weather.name
-                }
-                $DataToSave += $WeatherData
+            $Weather = Get-WeatherCurrentRaw -City $_City -ApiKey $weathervars.APIKey -Units imperial
+            $WeatherData = @{
+                'Longitude' = $Weather.coord.lon
+                'Latitude' = $Weather.coord.lat
+                'Description' = $Weather.weather.main
+                'Description_Detail' = $Weather.weather.description
+                'Temperature' = $Weather.main.temp
+                'Pressure' = $Weather.main.pressure
+                'Humidity' = $Weather.main.humidity
+                'Wind_Speed' = $Weather.wind.speed
+                'Wind_Degree' = $Weather.wind.deg
+                'Location' = $Weather.name
             }
-            catch
-            {
-                Write-Exception -Exception $_ -Stream Warning
-            }
+            $DataToSave += $WeatherData
         }
-
-        Write-LogAnalyticsLogEntry -WorkspaceId $GlobalVars.WorkspaceId -Key $Key -Data $DataToSave -LogType 'OpenWeather_CL'
-        Start-Sleep -Seconds 10
+        catch
+        {
+            Write-Exception -Exception $_ -Stream Warning
+        }
     }
 
+    Write-LogAnalyticsLogEntry -WorkspaceId $GlobalVars.WorkspaceId -Key $Key -Data $DataToSave -LogType 'OpenWeather_CL'
 }
 Catch
 {
