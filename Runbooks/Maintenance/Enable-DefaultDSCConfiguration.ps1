98e68cc9d6b5d4bc25b02b3e98292db2af5059e8
Ryan Andorfer <ryan@microsoft.com>
2017-01-13 08:55:35 -0600

    make a watcheraction

---
+++ Runbooks/Maintenance/Enable-DefaultDSCConfiguration.ps1
@@ -0,0 +1,56 @@
+﻿<#
+    .SYNOPSIS
+       Add a synopsis here to explain the PSScript. 
+
+    .Description
+        Give a description of the Script.
+
+#>
+Param(
+    $EventData
+)
+
+Import-Module SCOrchDev-Utility -Verbose:$False
+Import-Module SCOrchDev-Exception -Verbose:$False
+Import-Module SCOrchDev-File -Verbose:$False
+Import-Module SCOrchDev-GitIntegration -Verbose:$False
+Import-Module AzureRM.Profile -Verbose:$False
+
+$ErrorActionPreference = [System.Management.Automation.ActionPreference]::Stop
+$CompletedParameters = Write-StartingMessage -CommandName Enable-DefaultDSCConfiguration.ps1 -String ($EventData | ConvertTo-Json)
+
+$GlobalVars = Get-BatchAutomationVariable -Prefix 'zzGlobal' `
+                                          -Name 'SubscriptionName',
+                                                'SubscriptionAccessCredentialName',
+                                                'ResourceGroupName',
+                                                'SubscriptionAccessTenant'
+
+$SubscriptionAccessCredential = Get-AutomationPSCredential -Name $GlobalVars.SubscriptionAccessCredentialName
+
+
+Try
+{
+    Connect-AzureRmAccount -Credential $SubscriptionAccessCredential `
+                           -SubscriptionName $GlobalVars.SubscriptionName `
+                           -Tenant $GlobalVars.SubscriptionAccessTenant
+
+    Register-AzureRmAutomationDscNode -AzureVMName $EventData.EventProperties.PropertyBag.VMName `
+                                      -AzureVMResourceGroup $EventData.EventProperties.PropertyBag.VMResourceGroup `
+                                      -AutomationAccountName $GlobalVars.AutomationAccountName `
+                                      -ResourceGroupName $GlobalVars.ResourceGroupName `
+                                      -NodeConfigurationName $Vars.NodeConfigurationName
+}
+Catch
+{
+    $Exception = $_
+    $ExceptionInfo = Get-ExceptionInfo -Exception $Exception
+    Switch ($ExceptionInfo.FullyQualifiedErrorId)
+    {
+        Default
+        {
+            Write-Exception $Exception -Stream Warning
+        }
+    }
+}
+
+Write-CompletedMessage @CompletedParameters
