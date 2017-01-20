f345f82a636e83db3b40730b14109f11ad2eeb1e
Ryan Andorfer <ryan@microsoft.com>
2017-01-20 16:15:22 -0600

    demo for MCS

---
+++ Runbooks/Demo/Invoke-MCSDemo.ps1
@@ -0,0 +1,57 @@
+﻿<#
+    .SYNOPSIS
+       Add a synopsis here to explain the PSScript. 
+
+    .Description
+        Give a description of the Script.
+
+#>
+Param(
+
+)
+
+Import-Module SCOrchDev-Utility -Verbose:$False
+Import-Module SCOrchDev-Exception -Verbose:$False
+Import-Module SCOrchDev-File -Verbose:$False
+Import-Module SCOrchDev-GitIntegration -Verbose:$False
+Import-Module AzureRM.Profile -Verbose:$False
+
+$ErrorActionPreference = [System.Management.Automation.ActionPreference]::Stop
+$CompletedParameters = Write-StartingMessage -CommandName Invoke-MCSDemo.ps1
+
+$GlobalVars = Get-BatchAutomationVariable -Prefix 'zzGlobal' `
+                                          -Name 'SubscriptionName',
+                                                'SubscriptionAccessCredentialName',
+                                                'ResourceGroupName',
+                                                'SubscriptionAccessTenant'
+
+$Vars = Get-BatchAutomationVariable -Prefix 'MCSDemo' `
+                                    -Name 'Person',
+                                          'People'
+
+$SubscriptionAccessCredential = Get-AutomationPSCredential -Name $GlobalVars.SubscriptionAccessCredentialName
+
+$DemoCred = Get-AutomationPSCredential -Name 'manderson@microsoft.com'
+Try
+{
+    Add-AzureRmAccount -Credential $SubscriptionAccessCredential `
+                       -SubscriptionName $GlobalVars.SubscriptionName `
+                       -TenantId $GlobalVars.SubscriptionAccessTenant `
+                       -ServicePrincipal | Out-Null
+
+    Write-Output -InputObject "Hello $($Vars.Person) from $($Vars.People)"
+}
+Catch
+{
+    $Exception = $_
+    $ExceptionInfo = Get-ExceptionInfo -Exception $Exception
+    Switch ($ExceptionInfo.FullyQualifiedErrorId)
+    {
+        Default
+        {
+            Write-Exception $Exception -Stream Warning
+        }
+    }
+}
+
+Write-CompletedMessage @CompletedParameters
