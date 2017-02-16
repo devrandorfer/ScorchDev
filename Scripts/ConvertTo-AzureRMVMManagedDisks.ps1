36b6c20a427441ccb1230ca0f50f6a49d91f9bc2
Ryan Andorfer <ryan@microsoft.com>
2017-02-16 07:57:14 -0600

    update ps modules

---
+++ Scripts/ConvertTo-AzureRMVMManagedDisks.ps1
@@ -0,0 +1,20 @@
+﻿Login-AzureRmAccount
+
+$AvailabilitySets = Find-AzureRmResource -ResourceType Microsoft.Compute/AvailabilitySets
+
+Foreach($_AvailabilitySets in $AvailabilitySets)
+{
+    $avSet =  Get-AzureRmAvailabilitySet -ResourceGroupName $_AvailabilitySets.ResourceGroupName -Name $_AvailabilitySets.Name
+
+    Update-AzureRmAvailabilitySet -AvailabilitySet $avSet -Managed
+
+    foreach($vmInfo in $avSet.VirtualMachinesReferences)
+    {
+       $vm =  Get-AzureRmVM -ResourceGroupName $_AvailabilitySets.ResourceGroupName | Where-Object {$_.Id -eq $vmInfo.id}
+
+       Stop-AzureRmVM -ResourceGroupName $_AvailabilitySets.ResourceGroupName -Name  $vm.Name -Force
+
+       ConvertTo-AzureRmVMManagedDisk -ResourceGroupName $_AvailabilitySets.ResourceGroupName -VMName $vm.Name
+
+    }
+}
