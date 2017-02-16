6c6320ae75db44e1097cf3ce1ef27971d989e20a
Ryan Andorfer <ryan@microsoft.com>
2017-02-16 08:28:41 -0600

    cleanup old modules

---
+++ Runbooks/Maintenance/Find-AzureRMVMToShutdown.ps1
@@ -0,0 +1,51 @@
+﻿<#
+    .SYNOPSIS
+       Add a synopsis here to explain the PSScript. 
+
+    .Description
+        Give a description of the Script.
+
+#>
+Param(
+
+)
+
+Import-Module SCOrchDev-Utility -Verbose:$False
+Import-Module SCOrchDev-Exception -Verbose:$False
+Import-Module SCOrchDev-File -Verbose:$False
+Import-Module SCOrchDev-GitIntegration -Verbose:$False
+Import-Module AzureRM.Profile -Verbose:$False
+
+$ErrorActionPreference = [System.Management.Automation.ActionPreference]::Stop
+$CompletedParameters = Write-StartingMessage -CommandName Find-AzureRMVMToShutdown.ps1
+
+$GlobalVars = Get-BatchAutomationVariable -Prefix 'zzGlobal' `
+                                          -Name 'SubscriptionName',
+                                                'SubscriptionAccessCredentialName',
+                                                'ResourceGroupName',
+                                                'SubscriptionAccessTenant'
+
+$SubscriptionAccessCredential = Get-AutomationPSCredential -Name $GlobalVars.SubscriptionAccessCredentialName
+
+
+Try
+{
+    Add-AzureRmAccount -Credential $SubscriptionAccessCredential `
+                       -SubscriptionName $GlobalVars.SubscriptionName `
+                       -TenantId $GlobalVars.SubscriptionAccessTenant `
+                       -ServicePrincipal | Out-Null
+}
+Catch
+{
+    $Exception = $_
+    $ExceptionInfo = Get-ExceptionInfo -Exception $Exception
+    Switch ($ExceptionInfo.FullyQualifiedErrorId)
+    {
+        Default
+        {
+            Write-Exception $Exception -Stream Warning
+        }
+    }
+}
+
+Write-CompletedMessage @CompletedParameters
