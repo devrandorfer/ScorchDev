afb972fd55d155ae9dcc29803d04e90304b9f16f
Ryan Andorfer <ryan@microsoft.com>
2017-02-27 11:51:22 -0600

    unescape

--- Tanium/Packages/Stop-ProcessByExecutablePath.ps1
+++ Tanium/Packages/Stop-ProcessByExecutablePath.ps1
@@ -6,9 +6,12 @@
 Param(
     $ExecutablePath
 )
+[Reflection.Assembly]::LoadWithPartialName("System.Web") | out-null
+$Path = [System.Web.HttpUtility]::UrlDecode($ExecutablePath)
+
 $Null = $(
     Get-WmiObject -Class Win32_Process `
-                  | ? { $_.ExecutablePath -eq $ExecutablePath } `
+                  | ? { $_.ExecutablePath -eq $Path } `
                   | % { Stop-Process -Id $_.ProcessId -Force }
     
 )
